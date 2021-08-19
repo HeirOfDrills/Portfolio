1a0655b30326b4ca5b9835bd7a196db9416cb992
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-18 20:38:33 -0500

    Clicking on the title will now return home

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,9 +1,13 @@
+import { useHistory } from "react-router-dom";
 import NavBar from "./NavBar";
 
 export default function Header() {
+  const history = useHistory();
   return (
     <header>
-      <h1 className="title-text">Trevor Glascock</h1>
+      <h1 className="title-text" onClick={() => history.push("/")}>
+        Trevor Glascock
+      </h1>
       <NavBar type="header" />
     </header>
   );
