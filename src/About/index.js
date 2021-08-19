07c3fd3dcdb371683e6f42a6e2d099c7fe1abf0f
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-18 21:01:02 -0500

    InfoCard Component

--- src/About/index.js
+++ src/About/index.js
@@ -1,27 +1,10 @@
-import glass from "../images/glass.png";
+import InfoCard from "./InfoCard";
 import "./index.css";
 
 export default function About() {
   return (
     <main>
-      <section id="about" className="">
-        <div className="info-card accent-border group ">
-          <div className="info-img accent-border item">
-            <img
-              src={glass}
-              alt="Stylized Purple-Toned headshot of Trevor Glascock: A Software Engineer."
-            />
-          </div>
-          <div className="info-body item-double">
-            <div className="info-title ">
-              <h2 className="">About Trevor</h2>
-            </div>
-            <div className="info-text ">
-              <p className="">I am a Software Engineer.</p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <InfoCard />
     </main>
   );
 }
