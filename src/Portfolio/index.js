07e2193876f2354317256e8df86b5e9fefea8320
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-20 12:24:50 -0500

    first step towards modular and scalable approach to portfolio list

--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+import ProjectList from "./ProjectList";
+import getProjects from "./Projects";
+
 export default function Portfolio() {
   return (
     <main>
-      <h1>Welcome to the Portfolio!</h1>
+      <h1 style={{ textAlign: "center" }}>Welcome to Trevor's Portfolio!</h1>
+      <ProjectList projects={getProjects()} />
     </main>
   );
 }
