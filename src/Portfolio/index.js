87432d2d85f8db63c5b82aa4eea2f642666c3313
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-20 12:33:24 -0500

    Removed placeholder text

--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -4,7 +4,6 @@ import getProjects from "./Projects";
 export default function Portfolio() {
   return (
     <main>
-      <h1 style={{ textAlign: "center" }}>Welcome to Trevor's Portfolio!</h1>
       <ProjectList projects={getProjects()} />
     </main>
   );
