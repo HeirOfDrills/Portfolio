e49903b89e7ec5d6cc2c80da9d73fd857a9a347f
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-03 17:39:46 -0500

    Added className conditonals to make CSS rules more specefic

--- src/Common/InfoCard/index.js
+++ src/Common/InfoCard/index.js
@@ -10,11 +10,20 @@ export default function InfoCard({ card }) {
     <CardBody key="card-body" body={body} />,
   ];
 
-  if (flip) content.push(content.shift()); //If it's a flipped card, the image happens after the body
+  let className = `info-card accent-border`;
+  if (flip) {
+    content.push(content.shift()); // If it's a flipped card, the image happens after the body
+    className += ` flipped`; // and add the flipped class
+  }
+
+  // This allows us to make sure that project cards have a 1:1 flex layout instead of the 1:2 of other cards
+  if (type.includes("project")) {
+    className += ` project`;
+  }
 
   return (
     <section id={type} className="">
-      <div className="info-card accent-border group ">{content}</div>
+      <div className={className}>{content}</div>
     </section>
   );
 }
