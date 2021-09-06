cb3ef576c745f82bec52837ac23d39a47000ded4
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-06 15:54:08 -0500

    added deployment link

--- src/Common/InfoCard/CardImage.js
+++ src/Common/InfoCard/CardImage.js
@@ -1,8 +1,10 @@
 export default function CardImage({ image }) {
-  const { src, alt } = image;
+  const { src, alt, href } = image;
   return (
     <div className="info-img accent-border item">
-      <img src={src} alt={alt} />
+      <a href={href}>
+        <img src={src} alt={alt} />
+      </a>
     </div>
   );
 }
