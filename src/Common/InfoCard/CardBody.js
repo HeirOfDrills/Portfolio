5f8f191f1ee035df338e126071c1b5df1b583fc3
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-06 17:33:31 -0500

    Fixed missing key issue

--- src/Common/InfoCard/CardBody.js
+++ src/Common/InfoCard/CardBody.js
@@ -4,14 +4,14 @@ export default function CardBody({ body }) {
   const links = [];
   if (frontend_repo)
     links.push(
-      <p>
+      <p key={links.length + 1}>
         <a href={frontend_repo}>View Frontend Source Code</a>
       </p>
     );
 
   if (backend_repo)
     links.push(
-      <p>
+      <p key={links.length + 1}>
         <a href={backend_repo}>View Backend Source Code</a>
       </p>
     );
